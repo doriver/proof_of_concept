2acd8600a828f16b52966ddc996a02091c330098
DESKTOP-I6L47K7\user <leesang@gro>
2025-10-25 14:24:27 +0900

    init(springboot): 의존성 Web만 추가한 SpringBoot

---
+++ springboot-poc/src/main/java/com/practice/poc/SpringbootPocApplication.java
@@ -0,0 +1,13 @@
+package com.practice.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPocApplication.class, args);
+	}
+
+}
---
+++ springboot-poc/src/test/java/com/practice/poc/SpringbootPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
